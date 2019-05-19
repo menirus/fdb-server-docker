af652b3e6d24edc2230e965667ef805b4021cca5
menirus <surya.cse.iitkgp@gmail.com>
2019-05-19 15:31:34 +0530

    [Testing] Update the public address in the fdbserver command

--- fdb.bash
+++ fdb.bash
@@ -3,7 +3,7 @@
 source /var/fdb/scripts/create_server_environment.bash
 create_server_environment
 source /var/fdb/.fdbenv
-echo "Starting FDB server on $PUBLIC_IP:31111"
-fdbserver --listen_address 0.0.0.0:$FDB_PORT --public_address $PUBLIC_IP:31111 \
+echo "Starting FDB server on 35.194.5.18:31111"
+fdbserver --listen_address 0.0.0.0:$FDB_PORT --public_address 35.194.5.18:31111 \
 	--datadir /var/fdb/data --logdir /var/fdb/logs \
 	--locality_zoneid=`hostname` --locality_machineid=`hostname` --class $FDB_PROCESS_CLASS
