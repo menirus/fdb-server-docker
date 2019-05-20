8e3ae0f94bb16fc55a2d4c916b4ab8059764f13a
Surya Teja Gora <surya.cse.iitkgp@gmail.com>
2019-05-20 14:28:41 +0530

    Update create cluster file

--- create_cluster_file.bash
+++ create_cluster_file.bash
@@ -7,7 +7,7 @@ function create_cluster_file() {
 	if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
 		echo "$FDB_CLUSTER_FILE_CONTENTS" > $FDB_CLUSTER_FILE
 	elif [[ -n $FDB_COORDINATOR ]]; then
-		if [[ "$FDB_NETWORKING_MODE" != "kubernetes" ]]; then
+		if [[ "$FDB_NETWORKING_MODE" == "kubernetes" ]]; then
 			coordinator_ip=$KUBE_NODE_EXTERNAL_IP
 			# Handle empty kube_node_external_ip here, if necessary
 			echo "kubernetes:kubernetes@$coordinator_ip:$FDB_PORT" > $FDB_CLUSTER_FILE
