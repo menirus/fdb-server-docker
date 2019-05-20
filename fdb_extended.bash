6132cb5957d3b88f354072dbecf7adba027e4923
Surya Teja Gora <surya.cse.iitkgp@gmail.com>
2019-05-20 15:32:03 +0530

    Update

--- fdb_extended.bash
+++ fdb_extended.bash
@@ -4,6 +4,7 @@ source /var/fdb/scripts/create_server_environment.bash
 create_server_environment
 source /var/fdb/.fdbenv
 echo "Starting FDB server on $PUBLIC_IP:$FDB_PORT"
+echo $(getent hosts $PUBLIC_IP)
 fdbserver --listen_address 0.0.0.0:$FDB_PORT --public_address $PUBLIC_IP:$FDB_PORT \
 	--datadir /var/fdb/data --logdir /var/fdb/logs \
 	--locality_zoneid=`hostname` --locality_machineid=`hostname` --class $FDB_PROCESS_CLASS
